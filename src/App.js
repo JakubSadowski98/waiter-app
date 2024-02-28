1a9c11ab81ab0b3a536a5ffa074423f4565ad6c2
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-02-28 11:28:04 +0100

    Add Redux Thunk & JSON-server package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { fetchTables } from './redux/tableReducer';
+import { Container, Button } from 'react-bootstrap';
+//import { fetchTables } from './redux/tableReducer';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
