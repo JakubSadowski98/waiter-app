04d06ac1a12e7d9ff64c172ccc886b22cfe40030
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-03-06 13:31:01 +0100

    Create components structure & add navigation to subpages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import Home from './components/pages/Home/Home';
+import NotFound from './components/pages/NotFound/NotFound';
+import WaiterApp from './components/pages/WaiterApp/WaiterApp';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
 //import { fetchTables } from './redux/tableReducer';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+//import { useDispatch } from 'react-redux';
+//import { useEffect } from 'react';
 
 const App = () => {
   return (
-    <div>
-      Hello word
-    </div>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/waiter" element={<WaiterApp />} /> 
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/components/pages/WaiterApp/WaiterApp.js
@@ -0,0 +1,9 @@
+const WaiterApp = () => {
+  return(
+    <div>
+      <h1>WaiterApp</h1>
+    </div>
+  );
+};
+
+export default WaiterApp;
