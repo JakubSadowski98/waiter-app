9da24badfeae21e404ddefba8035cc323d87f55b
Jakub Sadowski <kuba.sadowsky@gmail.com>
2024-03-11 19:23:27 +0100

    Add navigation to Home subpage

--- src/components/features/TableDetails/TableDetails.js
+++ src/components/features/TableDetails/TableDetails.js
@@ -1,15 +1,16 @@
 import styles from './TableDetails.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { getTableById, editTable } from '../../../redux/tablesRedux';
+import { useDispatch } from 'react-redux';
+import { editTable } from '../../../redux/tablesRedux';
 import { useEffect, useState } from 'react';
-import { useParams, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import { Row, Col, Button, Form as ReactForm } from "react-bootstrap";
 
-const TableDetails = () => {
-  const { tableId } = useParams(); // przypisanie do stałej tableId dynamicznego parametru adresu
-  const singleTable = useSelector(state => getTableById(state, tableId)); // przypsianie do stałej singleTable obiektu-stolika o danym id
+const TableDetails = prop => {
+  const singleTable = prop.singleTable;
+  
   const dispatch = useDispatch(); // funkcja, która jest pośrednikiem do włączenia funkcji reducer, która z kolei włącza wszystkie subreducery
+  const navigate = useNavigate();
 
   const [status, setStatus] = useState(singleTable.status);
   const [maxPeopleAmount, setMaxPeopleAmount]= useState(singleTable.maxPeopleAmount);
@@ -27,9 +28,9 @@ const TableDetails = () => {
     const parsedMaxPeopleAmount = validationAndParseData(maxPeopleAmount);
     const parsedBill = validationAndParseData(bill);
 
-    const updatedBill = status !== "Busy" ? 0 : parsedBill; // jeśli status jest inny niż "Busy" to kwota rachunku wynosi 0
-    const updatedPeopleAmount = status === "Free" || status === "Cleaning" ? 0 : parsedPeopleAmount;
-    const updatedMaxPeopleAmount = status === "Free" || status === "Cleaning" ? 0 : parsedMaxPeopleAmount;
+    const updatedBill = status !== "busy" ? 0 : parsedBill; // jeśli status jest inny niż "Busy" to kwota rachunku wynosi 0
+    const updatedPeopleAmount = (status === "free" || status === "cleaning") ? 0 : parsedPeopleAmount;
+    const updatedMaxPeopleAmount = (status === "free" || status === "cleaning") ? 0 : parsedMaxPeopleAmount;
 
     const thisTable = {
       id: singleTable.id,
@@ -38,30 +39,25 @@ const TableDetails = () => {
       maxPeopleAmount: updatedMaxPeopleAmount,
       bill: updatedBill,
     };
-    //console.log(thisTable) - poprawne dane
     dispatch(editTable(thisTable)); // uruchamia reducera, a ten z kolei uruchamia tablesReducer
+    navigate('/'); // nawigowanie do podstrony home
   }
 
-  useEffect(() => { 
+  useEffect(() => { // wykonanie instrukcji z arg1, gdy dojdzie do zmiany, któregoś ze stanów w arg2
     const peopleAmountValue = parseInt(peopleAmount);
     const maxPeopleAmountValue = parseInt(maxPeopleAmount);
+    const billValue = parseInt(bill);
 
-    if (peopleAmountValue > maxPeopleAmountValue){
-        setPeopleAmount(maxPeopleAmountValue)
-    };
-
-    if (peopleAmountValue < 0 || isNaN(peopleAmountValue)) {
-        setPeopleAmount(0);
-    }
-
-    if (maxPeopleAmountValue < 0 || isNaN(maxPeopleAmountValue)){
-        setMaxPeopleAmount(0);
-    }},
-    [peopleAmount, maxPeopleAmount]
+    if (peopleAmountValue > maxPeopleAmountValue) setPeopleAmount(maxPeopleAmountValue)
+    if ((peopleAmountValue < 0) || isNaN(peopleAmountValue)) setPeopleAmount(0);
+        
+    if ((maxPeopleAmountValue < 0) || isNaN(maxPeopleAmountValue)) setMaxPeopleAmount(0);
+        
+    if (isNaN(billValue)) setBill(0);
+  },
+    [peopleAmount, maxPeopleAmount, bill]
   );
 
-  // nawigowanie do podstrony Home jeśli podano zły adres
-  // if(!singleTable) return <Navigate to="/" />  NIE DZIAŁA
   return (
     <div>
       <h1>Table {singleTable.id}</h1>
@@ -72,17 +68,17 @@ const TableDetails = () => {
               <strong>Status: </strong>
             </Form.Label>
             <Col sm={3}>
-              <Form.Select className={styles.input_select} value= {status} onChange={ event => setStatus(event.target.value)} aria-label="Default select example">
-                <option value="Free">Free</option>
-                <option value="Busy">Busy</option>
-                <option value="Reserved">Reserved</option>
-                <option value="Cleaning">Cleaning</option>
+              <Form.Select className={styles.input_select} value= {status} onChange={ e => {setStatus(e.target.value); setPeopleAmount(0); setMaxPeopleAmount(4); setBill(0)}} aria-label="Default select example">
+                <option value="free">Free</option>
+                <option value="busy">Busy</option>
+                <option value="reserved">Reserved</option>
+                <option value="cleaning">Cleaning</option>
               </Form.Select>
             </Col>
           </Row>
         </Form.Group>
 
-        { (status === "Busy" || status === "Reserved") && (
+        { (status === "busy" || status === "reserved") && (
         <Form.Group>
           <Row className="mb-3 align-items-center">
             <Form.Label column sm={1}>
@@ -90,23 +86,23 @@ const TableDetails = () => {
             </Form.Label>
             <Col sm={2}>
               <div className="d-flex align-items-center">
-                <Form.Control type='number' className={styles.input_people} min='0' max={maxPeopleAmount} value={peopleAmount} placeholder='Table peopleAmount...' onChange={e => setPeopleAmount(e.target.value)} />
+                <Form.Control type='number' className={styles.input_people} min='0' max={maxPeopleAmount} value={peopleAmount} onChange={e => setPeopleAmount(e.target.value)} />
                 <span className="mx-2">/</span>
-                <Form.Control type='number' className={styles.input_people} max="10" value={maxPeopleAmount} placeholder='Table maxPeopleAmount...' onChange={e => setMaxPeopleAmount(e.target.value)} />
+                <Form.Control type='number' className={styles.input_people} max="10" value={maxPeopleAmount} onChange={e => setMaxPeopleAmount(e.target.value)} />
               </div>
             </Col>
           </Row>
         </Form.Group>
         )}
         
-        { status === "Busy" && ( // jeśli status jest inny niż "Busy" to inforamcja o kwocie rachunku jest schowana
+        { status === "busy" && ( // jeśli status jest inny niż "Busy" to inforamcja o kwocie rachunku jest schowana
           <Form.Group>
             <Row className='mb-3'>
               <Form.Label column sm={1} htmlFor="bill">
                 <strong>Bill: </strong>
               </Form.Label>
               <Col sm={1}>
-                <Form.Control id="bill" type='number' className={styles.input_bill} value={bill} placeholder="Table bill" onChange={e => setBill(e.target.value)} />
+                <Form.Control id="bill" type='number' className={styles.input_bill} min='0' value={bill} onChange={e => setBill(e.target.value)} />
               </Col>
             </Row>      
           </Form.Group>
--- src/components/pages/WaiterApp/WaiterApp.js
+++ src/components/pages/WaiterApp/WaiterApp.js
@@ -1,11 +1,21 @@
 import TableDetails from "../../features/TableDetails/TableDetails";
+import { useParams, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getTableById } from '../../../redux/tablesRedux';
 
 const WaiterApp = () => {
-  return(
-    <div>
-      <TableDetails />
-    </div>
-  );
+  const { tableId } = useParams(); // przypisanie do stałej tableId dynamicznego parametru adresu
+  const singleTable = useSelector(state => getTableById(state, tableId)); // przypsianie do stałej singleTable obiektu-stolika o danym id
+  
+  if (!singleTable) { // nawigowanie do podstrony Home jeśli podano zły adres
+    return <Navigate to="/" />;
+  } else {
+    return(
+      <div>
+        <TableDetails singleTable={singleTable}/>
+      </div>
+    );
+  }
 };
 
 export default WaiterApp;
